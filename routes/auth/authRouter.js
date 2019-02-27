1bde1a0f077822a2103bd509c1553e33fd519244
Shalitha Suranga <shalithas@99x.lk>
2019-02-27 12:28:22 +0530

    Add auth middleware

--- routes/auth/authRouter.js
+++ routes/auth/authRouter.js
@@ -1,10 +1,14 @@
 let express = require('express');
+let jwt = require('express-jwt');
 let authController = require('../../controllers/authController');
 let router = express.Router();
+const config = require('../../config');
 
 
 router.post('/api/v1/login', authController.login);
-router.post('/api/v1/me', authController.me);
+router.post('/api/v1/me', jwt({
+    secret : config.secret
+}) ,authController.me);
 
 
 module.exports = router;
